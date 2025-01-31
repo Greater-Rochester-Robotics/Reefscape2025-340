19cec95cfb1ab500bf3b8cb4893cd78640d2d59d
Caleb-J-Wallace <calebjasonwallace@gmail.com>
2025-01-30 19:18:54 -0500

    Formatting changes

--- src/main/java/org/team340/robot/subsystems/GooseneckPivot.java
+++ src/main/java/org/team340/robot/subsystems/GooseneckPivot.java
@@ -1,36 +1,36 @@
 package org.team340.robot.subsystems;
 
+import com.ctre.phoenix6.configs.HardwareLimitSwitchConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.CANdi;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.ForwardLimitSourceValue;
+import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.wpilibj2.command.Command;
 import java.util.function.DoubleSupplier;
-
 import org.team340.lib.util.Tunable;
 import org.team340.lib.util.Tunable.TunableDouble;
 import org.team340.lib.util.command.GRRSubsystem;
 import org.team340.lib.util.vendors.PhoenixUtil;
 import org.team340.robot.Constants.RobotMap;
 
-import com.ctre.phoenix6.configs.HardwareLimitSwitchConfigs;
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.hardware.CANdi;
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.ForwardLimitSourceValue;
-
-import edu.wpi.first.epilogue.Logged;
-import edu.wpi.first.wpilibj2.command.Command;
-
 @Logged
 public class GooseneckPivot extends GRRSubsystem {
+
     private static final TunableDouble kDeployedPosition = Tunable.doubleValue("GooseneckPivot/kDeployedPosition", 0.0);
     private static final TunableDouble kSafePosition = Tunable.doubleValue("GooseneckPivot/kSafePosition", 0.0);
-    
+
     private final TalonFX pivotMotor;
     private final CANdi pivotEncoder;
 
     public GooseneckPivot() {
         pivotMotor = new TalonFX(RobotMap.kGooseneckPivotMotor);
         TalonFXConfiguration pivotConfig = new TalonFXConfiguration();
-        
+
         PhoenixUtil.run("Clear pivotMotor sticky faults", pivotMotor, () -> pivotMotor.clearStickyFaults());
-        PhoenixUtil.run("Apply pivotMotor TalonFXConfiguration", pivotMotor, () -> pivotMotor.getConfigurator().apply(pivotConfig));
+        PhoenixUtil.run("Apply pivotMotor TalonFXConfiguration", pivotMotor, () ->
+            pivotMotor.getConfigurator().apply(pivotConfig)
+        );
 
         pivotEncoder = new CANdi(RobotMap.kGooseneckPivotEncoder);
 
@@ -38,7 +38,9 @@ public class GooseneckPivot extends GRRSubsystem {
         limitConfigs.ForwardLimitSource = ForwardLimitSourceValue.RemoteCANcoder;
         limitConfigs.ForwardLimitRemoteSensorID = pivotEncoder.getDeviceID();
 
-        PhoenixUtil.run("Apply pivotMotor HardwareLimitSwitchConfigs", pivotMotor, () -> pivotMotor.getConfigurator().apply(limitConfigs));
+        PhoenixUtil.run("Apply pivotMotor HardwareLimitSwitchConfigs", pivotMotor, () ->
+            pivotMotor.getConfigurator().apply(limitConfigs)
+        );
     }
 
     private void applyPosition(double position) {
