c6cdb49271d3eb240f12549a7b039d08428aa208
Bryce Roethel <me@bryceroethel.com>
2025-02-04 12:27:53 -0500

    Cache status signals and cleanup Elevator
    Caching status signals is the recommended paradigm for interacting with CTRE hardware. Left some TODOs as well

--- src/main/java/org/team340/robot/subsystems/Elevator.java
+++ src/main/java/org/team340/robot/subsystems/Elevator.java
@@ -1,11 +1,13 @@
 package org.team340.robot.subsystems;
 
+import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.ReverseLimitTypeValue;
 import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj2.command.Command;
 import org.team340.lib.util.Math2;
 import org.team340.lib.util.Tunable;
@@ -37,6 +39,9 @@ public class Elevator extends GRRSubsystem {
 
     private final TalonFX leadMotor;
     private final TalonFX followMotor;
+
+    private final StatusSignal<Angle> leadPosition;
+    private final StatusSignal<Angle> followPosition;
     private final MotionMagicVoltage positionControl;
 
     public Elevator() {
@@ -44,34 +49,38 @@ public class Elevator extends GRRSubsystem {
         leadMotor = new TalonFX(RobotMap.kElevatorLead, RobotMap.kLowerCANBus);
         followMotor = new TalonFX(RobotMap.kElevatorFollow, RobotMap.kLowerCANBus);
 
-        TalonFXConfiguration motorCfg = new TalonFXConfiguration();
+        TalonFXConfiguration config = new TalonFXConfiguration();
 
-        motorCfg.CurrentLimits.StatorCurrentLimit = 80;
-        motorCfg.CurrentLimits.SupplyCurrentLimit = 100;
+        config.CurrentLimits.StatorCurrentLimit = 80;
+        config.CurrentLimits.SupplyCurrentLimit = 100;
 
-        motorCfg.Slot0.kP = 0;
-        motorCfg.Slot0.kI = 0;
-        motorCfg.Slot0.kD = 0;
-        motorCfg.Slot0.kG = 0;
-        motorCfg.Slot0.kS = 0;
-        motorCfg.Slot0.kV = 0;
+        config.Slot0.kP = 0;
+        config.Slot0.kI = 0;
+        config.Slot0.kD = 0;
+        config.Slot0.kG = 0;
+        config.Slot0.kS = 0;
+        config.Slot0.kV = 0;
 
-        motorCfg.MotionMagic.MotionMagicCruiseVelocity = 0;
-        motorCfg.MotionMagic.MotionMagicAcceleration = 0;
+        config.MotionMagic.MotionMagicCruiseVelocity = 0;
+        config.MotionMagic.MotionMagicAcceleration = 0;
 
-        motorCfg.HardwareLimitSwitch.ReverseLimitSource = RobotMap.kElevatorLimitPort;
-        motorCfg.HardwareLimitSwitch.ReverseLimitRemoteSensorID = RobotMap.kElevatorCANdi;
-        motorCfg.HardwareLimitSwitch.ReverseLimitType = ReverseLimitTypeValue.NormallyOpen;
-        motorCfg.HardwareLimitSwitch.ReverseLimitAutosetPositionEnable = true;
-        motorCfg.HardwareLimitSwitch.ReverseLimitAutosetPositionValue = 0;
+        config.HardwareLimitSwitch.ReverseLimitSource = RobotMap.kElevatorLimitPort;
+        config.HardwareLimitSwitch.ReverseLimitRemoteSensorID = RobotMap.kElevatorCANdi;
+        config.HardwareLimitSwitch.ReverseLimitType = ReverseLimitTypeValue.NormallyOpen;
+        config.HardwareLimitSwitch.ReverseLimitAutosetPositionEnable = true;
+        config.HardwareLimitSwitch.ReverseLimitAutosetPositionValue = 0;
 
-        PhoenixUtil.run("Clear Lead Motor Sticky Faults", leadMotor, () -> leadMotor.clearStickyFaults());
-        PhoenixUtil.run("Clear Follow Motor sticky Faults", followMotor, () -> followMotor.clearStickyFaults());
-        PhoenixUtil.run("Apply Lead TalonFXConfiguration", leadMotor, () -> leadMotor.getConfigurator().apply(motorCfg)
+        PhoenixUtil.run("Clear Elevator Lead Sticky Faults", leadMotor, () -> leadMotor.clearStickyFaults());
+        PhoenixUtil.run("Clear Elevator Follow Sticky Faults", followMotor, () -> followMotor.clearStickyFaults());
+        PhoenixUtil.run("Apply Elevator Lead TalonFXConfiguration", leadMotor, () ->
+            leadMotor.getConfigurator().apply(config)
         );
-        PhoenixUtil.run("Apply Follow TalonFXConfiguration", followMotor, () ->
-            followMotor.getConfigurator().apply(motorCfg)
+        PhoenixUtil.run("Apply Elevator Follow TalonFXConfiguration", followMotor, () ->
+            followMotor.getConfigurator().apply(config)
         );
+
+        leadPosition = leadMotor.getPosition();
+        followPosition = followMotor.getPosition();
 
         positionControl = new MotionMagicVoltage(0).withSlot(0);
         followMotor.setControl(new Follower(leadMotor.getDeviceID(), false));
@@ -80,12 +89,15 @@ public class Elevator extends GRRSubsystem {
     // *************** Helper Functions ***************
 
     public boolean isAtPosition(ElevatorPosition position) {
-        double averagePosition =
-            (leadMotor.getPosition().getValueAsDouble() + followMotor.getPosition().getValueAsDouble()) / 2.0;
+        // TODO Latency compensation
+        double averagePosition = (leadPosition.getValueAsDouble() + followPosition.getValueAsDouble()) / 2.0;
+
+        // TODO Define epsilon
         return Math2.epsilonEquals(averagePosition, position.rotations());
     }
 
     // *************** Commands ***************
+
     /**
      * Move elevator to predetermined positions.
      * @param position - The predetermined positions.
@@ -96,7 +108,7 @@ public class Elevator extends GRRSubsystem {
                 // Setting lead motor also sets the follow motor
                 leadMotor.setControl(positionControl.withPosition(position.rotations()));
             })
-            .isFinished(() -> isAtPosition(position))
+            .isFinished(() -> isAtPosition(position)) // TODO maybe remove for scheduling purposes?
             .onEnd(() -> {
                 leadMotor.stopMotor();
             });
